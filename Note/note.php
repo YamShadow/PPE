8773aee9da105322fcb5fe243fe2bc5250d925b5
eleve <eleve@S302-12.S309.sio.lms>
2015-09-14 17:14:35 +0200

    mise a jour note

--- Note/note.php
+++ Note/note.php
@@ -1,8 +1,23 @@
-<?php
+ToDoList
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Mission 1 :
+
+Mehmet:
+- Creation table "Administration" pour les comptables. (Pareil que Visiteur + Rang)
+- modifier le fichier de recherche de connexion pour qu'il parcourt aussi la table Administration en plus de la table visiteur
+- Mot de passe à crypter (sha1).
+
+Mathieu :
+- Quand connecté, menu avec :
+bouton Vert / rouge quand création de fiche => CR et Cl 
+Bouton fiche validé et mise en payent ( orange) => VA
+bouton remboursé ( blanche ) = Historique => RB ou Refusé
+Plus de détails sur le visiteur (dans le tableau du mois)
+- changement du tableau avec un select onChange()
+
+Mission 2 :
+ 
+
+
+
 
