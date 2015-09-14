28c229b6a3fdffc3cc23260ba15a0526127b9c21
YamShadow <mathieu.nibas@gmail.com>
2015-09-14 17:30:34 +0200

    Version modifier de la base de données

--- EbaucheCode/Cryptagemdp.php
+++ EbaucheCode/Cryptagemdp.php
@@ -1,22 +1,15 @@
 <?php
     require("fct.ini.php");
     $con = connexion();
-
 $sql= "Select * from visiteur";
 $resultat = $con->query($sql);
-
- while($visiteur= $resultat->fetch(PDO::FETCH_OBJ)) {
-     
+ while($visiteur= $resultat->fetch(PDO::FETCH_OBJ)) {   
+     $id = $visiteur->id;
      $mdp = $visiteur->mdp;
      $nom = $visiteur->nom;
      $prenom = $visiteur->prenom;
-     
-    
     $mdpcryp = sha1($mdp);
     
-    echo "Membre ".$nom." ".$prenom." avec le mdp :".$mdp."<br/>";
-    echo "New mot de passe crypté: ".$mdpcryp."<br/>";
-    echo "<br/>";
-     
-     
+$sql2 = "update visiteur set mdp='$mdpcryp' where id='$id'";
+$res = $con->exec($sql2);   
  }
